0de406d98eaf3ededc7e67af15812d3976010598
sooraav <purushothaman.sourav4@gmail.com>
2024-06-29 22:07:34 +0530

    search implemented for title

--- MovieDatabase/Interface/Home/HomeView.swift
+++ MovieDatabase/Interface/Home/HomeView.swift
@@ -10,27 +10,40 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var viewModel = HomeViewModel()
-    
+   
     var body: some View {
-       
-        List(viewModel.categories, id: \.type) { row in
-            DisclosureGroup {
-                ForEach(row.subCategories) { subCategory in
-                    
-                    DisclosureGroup {
-                        
-                        ForEach(subCategory.movies) { movie in
-                            Text(movie.title)
+        NavigationStack {
+            VStack {
+                if viewModel.searchText.isEmpty {
+                    List(viewModel.categories, id: \.type) { row in
+                        DisclosureGroup {
+                            ForEach(row.subCategories) { subCategory in
+                                
+                                DisclosureGroup {
+                                    
+                                    ForEach(subCategory.movies) { movie in
+                                        Text(movie.title)
+                                    }
+                                } label: {
+                                    Text( subCategory.item)
+                                }
+                                
+                            }
+                        } label: {
+                            Text(row.type.rawValue)
                         }
-                    } label: {
-                        Text( subCategory.item)
+                        
                     }
-
+                } else {
+                    
+                    List(viewModel.searchResuls) { movie in
+                        
+                        Text(movie.title)
+                    }
                 }
-            } label: {
-                Text(row.type.rawValue)
             }
-           
+            .navigationTitle("Movies")
+            .searchable(text: $viewModel.searchText)
         }
     }
 }
--- MovieDatabase/Interface/Home/HomeViewModel.swift
+++ MovieDatabase/Interface/Home/HomeViewModel.swift
@@ -13,6 +13,20 @@ import SwiftUI
     var categories = [Category]()
     private var movies = [Movie]()
     var dataFetcher: DataFetchable = DataManager()
+    var searchText: String = ""
+    var searchResuls: [Movie] {
+        
+        if searchText.isEmpty {
+            
+            return movies
+        } else {
+            
+            return movies.filter {
+                
+                $0.title.contains(searchText)
+            }
+        }
+    }
     
     init() {
         
@@ -21,49 +35,51 @@ import SwiftUI
     
     func getData() {
         
-        if let movies = dataFetcher.getMovieData() {
-            
-            self.movies =  movies
-            
-            var yearSubCategories: [String: [Movie]] = [:]
-            var genreSubCategories: [String: [Movie]] = [:]
-            var directorSubCategories: [String: [Movie]] = [:]
-            var actorSubCategories: [String: [Movie]] = [:]
-
-            // Populate the dictionaries
-            for movie in movies {
-                // By Year
-                let yearKey = String(movie.year)
-                if yearSubCategories[yearKey] == nil {
-                    yearSubCategories[yearKey] = []
-                }
-                yearSubCategories[yearKey]?.append(movie)
+        Task.detached(priority: .high) { [self] in
+            if let movies = dataFetcher.getMovieData() {
                 
-                // By Genre
-                if genreSubCategories[movie.genre] == nil {
-                    genreSubCategories[movie.genre] = []
-                }
-                genreSubCategories[movie.genre]?.append(movie)
+                self.movies =  movies
                 
-                // By Director
-                if directorSubCategories[movie.director] == nil {
-                    directorSubCategories[movie.director] = []
-                }
-                directorSubCategories[movie.director]?.append(movie)
+                var yearSubCategories: [String: [Movie]] = [:]
+                var genreSubCategories: [String: [Movie]] = [:]
+                var directorSubCategories: [String: [Movie]] = [:]
+                var actorSubCategories: [String: [Movie]] = [:]
                 
-                // By Actor
-                
-                if actorSubCategories[movie.actors] == nil {
-                    actorSubCategories[movie.actors] = []
+                // Populate the dictionaries
+                for movie in movies {
+                    // By Year
+                    let yearKey = String(movie.year)
+                    if yearSubCategories[yearKey] == nil {
+                        yearSubCategories[yearKey] = []
+                    }
+                    yearSubCategories[yearKey]?.append(movie)
+                    
+                    // By Genre
+                    if genreSubCategories[movie.genre] == nil {
+                        genreSubCategories[movie.genre] = []
+                    }
+                    genreSubCategories[movie.genre]?.append(movie)
+                    
+                    // By Director
+                    if directorSubCategories[movie.director] == nil {
+                        directorSubCategories[movie.director] = []
+                    }
+                    directorSubCategories[movie.director]?.append(movie)
+                    
+                    // By Actor
+                    
+                    if actorSubCategories[movie.actors] == nil {
+                        actorSubCategories[movie.actors] = []
+                    }
+                    actorSubCategories[movie.actors]?.append(movie)
                 }
-                actorSubCategories[movie.actors]?.append(movie)
+                
+                categories.append(Category(type: .year, subCategories: convertToSubCategories(from: yearSubCategories)))
+                categories.append(Category(type: .genre, subCategories: convertToSubCategories(from: genreSubCategories)))
+                categories.append(Category(type: .actors, subCategories: convertToSubCategories(from: actorSubCategories)))
+                categories.append(Category(type: .director, subCategories: convertToSubCategories(from: directorSubCategories)))
+                categories.append(Category(type: .allMovies, subCategories: [SubCategory(item: "allMovies", movies: movies)]))
             }
-            
-            categories.append(Category(type: .year, subCategories: convertToSubCategories(from: yearSubCategories)))
-            categories.append(Category(type: .genre, subCategories: convertToSubCategories(from: genreSubCategories)))
-            categories.append(Category(type: .actors, subCategories: convertToSubCategories(from: actorSubCategories)))
-            categories.append(Category(type: .director, subCategories: convertToSubCategories(from: directorSubCategories)))
-            categories.append(Category(type: .allMovies, subCategories: [SubCategory(item: "allMovies", movies: movies)]))
         }
     }
     
