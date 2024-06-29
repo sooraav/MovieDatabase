6234b52b18d757b8410794fe7cc8291541e4af2f
sooraav <purushothaman.sourav4@gmail.com>
2024-06-29 13:39:43 +0530

    initial Commit

---
+++ MovieDatabase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDatabase
+//
+//  Created by Sourav on 29/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
